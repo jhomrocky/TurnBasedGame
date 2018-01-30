8b31addd7d83e7051437170f0b668fdd8a705cbf
Joseph H <jhomrocky@gmail.com>
2018-01-30 18:44:21 -0500

    Added computer move functionality

--- Main.py
+++ Main.py
@@ -11,4 +11,46 @@ Give each move a name.
 """
 
 from random import randint
+# both user and computer start with 100 HP
+user_hp = 100
+computer_hp = 100
+
+
+
+# for showing HP of both computer and user
+def show_status():
+    print("Current status:\nUser HP: %s\nComputer HP: %s" % (user_hp, computer_hp))
+
+# for executing a hit/heal for user
+def user_moves():
+
+
+# for figuring out what move computer will use
+def computer_move():
+    global user_hp, computer_hp
+# make sure to include if hp < 35% (or something) then up the probability to heal
+    comp_move_calc = randint(1, 100)
+    comp_light_hit_dmg = randint(18-25) # low range, moderate damage
+    comp_heavy_hit_dmg = randint(10-35) # high range, high damage (can also be very low)
+    comp_heal_amount = randint(18-25)
+    # moves if comp hp > 35%
+    if computer_hp > 35:
+        # 30% chance to use light hit, 30% chance for other hit, 40% chance to heal
+        if comp_move_calc in range(1,30):
+            user_hp -= comp_light_hit_dmg
+        elif comp_move_calc in range(31,60):
+            user_hp -= comp_heavy_hit_dmg
+        else:
+            computer_hp += comp_heal_amount
+    elif 0 < computer_hp < 35:
+        if comp_move_calc in range(1, 20):
+            user_hp -= comp_light_hit_dmg
+        elif comp_move_calc in range(21, 40):
+            user_hp -= comp_heavy_hit_dmg
+        else:
+            computer_hp += comp_heal_amount
+
+
+
+
 
